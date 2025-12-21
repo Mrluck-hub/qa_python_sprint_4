f9f1549ff0cb4735c3715e3cafc8800472f6a501
Vyacheslav Melnikov <robber2005@mail.ru>
2025-12-21 20:10:11 +0300

    Created a check that the books_genre dictionary is initially empty

--- tests.py
+++ tests.py
@@ -3,7 +3,11 @@ import pytest
 from main import BooksCollector
 
 class TestBooksCollector:
+    
     @pytest.fixture
     def collector(self):
         return BooksCollector()
+    
+    def test_init_books_genre_is_empty(self, collector):
+        assert collector.books_genre == {}
     
