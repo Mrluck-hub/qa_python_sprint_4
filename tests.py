5c267e41aac5660de5999293dbc2099efc5b4adb
Vyacheslav Melnikov <robber2005@mail.ru>
2025-12-21 20:12:33 +0300

    Created a check for adding a book with a name length of 40 characters

--- tests.py
+++ tests.py
@@ -11,3 +11,7 @@ class TestBooksCollector:
     def test_init_books_genre_is_empty(self, collector):
         assert collector.books_genre == {}
     
+    def test_add_new_book_name_length_40_is_added(self, collector):
+        long_name = 'a'*40
+        collector.add_new_book(long_name)
+        assert long_name in collector.books_genre
