69c4cf3a2de27a3f24b8e7ae28d342045719004a
Vyacheslav Melnikov <robber2005@mail.ru>
2025-12-21 20:22:17 +0300

    Created a check to ensure that a genre cannot be assigned to a book that is not in the collection

--- tests.py
+++ tests.py
@@ -15,3 +15,7 @@ class TestBooksCollector:
         long_name = 'a'*40
         collector.add_new_book(long_name)
         assert long_name in collector.books_genre
+
+    def test_set_book_genre_to_non_existent_book(self, collector):
+        collector.set_book_genre('Несуществующая книга', 'Ужасы')
+        assert 'Несуществующая книга' not in collector.get_books_genre()
