6068bed390195b153eff5052b93b679d14a068ce
Vyacheslav Melnikov <robber2005@mail.ru>
2025-12-24 19:36:06 +0300

    Move the fixture to conftest.py

--- tests.py
+++ tests.py
@@ -1,13 +1,5 @@
-import pytest
-
-from main import BooksCollector
-
 class TestBooksCollector:
-    
-    @pytest.fixture
-    def collector(self):
-        return BooksCollector()
-    
+        
     def test_init_books_genre_is_empty(self, collector):
         assert collector.books_genre == {}
     
