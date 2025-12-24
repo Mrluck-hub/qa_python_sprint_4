3ae3f55195864c8a4623289ab44b2bd0cbd95bd9
Vyacheslav Melnikov <robber2005@mail.ru>
2025-12-24 20:15:32 +0300

    Remove improper test

--- tests.py
+++ tests.py
@@ -1,8 +1,5 @@
 class TestBooksCollector:
-        
-    def test_init_books_genre_is_empty(self, collector):
-        assert collector.books_genre == {}
-    
+          
     def test_add_new_book_name_length_40_is_added(self, collector):
         long_name = 'a'*40
         collector.add_new_book(long_name)
